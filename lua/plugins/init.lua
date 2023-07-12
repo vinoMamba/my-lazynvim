34256bc4b3600813d9259188dcd7651dd3b1ca5d
wangxin <vino0908@outlook.com>
2023-07-12 18:16:49 +0800

    配置主题，修改LSP快捷键等

---
+++ lua/plugins/init.lua
@@ -0,0 +1,60 @@
+return {
+  ------------------disable plugins eg-------------------------
+  -- disable plugins
+  -- { "folke/trouble.nvim", enabled = false }
+  --------------------------------------------------------------
+  --
+  --
+  ------------Adding & Disabling Plugin Keymaps eg-------------
+  -- {
+  --   "nvim-telescope/telescope.nvim",
+  --   keys = {
+  --     -- disable the keymap to grep files
+  --     { "<leader>/",  false },
+  --     -- change a keymap
+  --     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+  --     -- add a keymap to browse plugin files
+  --     {
+  --       "<leader>fp",
+  --       function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+  --       desc = "Find Plugin File",
+  --     },
+  --   },
+  -- },
+  --------------------------------------------------------------
+
+  { "theniceboy/nvim-deus" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "deus",
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- disable a keymap
+      keys[#keys + 1] = { "K", false }
+      -- add a keymap
+      keys[#keys + 1] = { "<tab>k", vim.lsp.buf.hover, desc = "Hover" }
+    end,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      { "<leader>e", false },
+      {
+        "<A-1>",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = require("lazyvim.util").get_root() })
+        end,
+      }
+    },
+    opts = {
+      window = {
+        position = "right",
+      }
+    }
+  }
+}
