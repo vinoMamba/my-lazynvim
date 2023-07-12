51e51826b8b7ddc6494f986b2fd5a082f9b6dba3
wangxin <vino0908@outlook.com>
2023-07-12 16:52:34 +0800

    修改 window split 的键位

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,11 +16,19 @@ end
 
 -- basic
 map({ "i" }, "jk", "<Esc>", { desc = "Quit Insert Mode", noremap = true }) -- buffers
-map({ "v", "n", "s" }, "S", "<cmd>w<cr><esc>", { desc = "Save file" })
-map({ "n" }, "Q", "<cmd>q<cr><esc>", { desc = "Quit" })
+map({ "v", "n", "s" }, "S", "<cmd>w<cr><esc>", { desc = "Save file", noremap = true })
+map({ "n" }, "Q", "<cmd>q<cr><esc>", { desc = "Quit", noremap = true })
 
-map({ "n" }, "K", "5k", { desc = "Quit" })
-map({ "n" }, "J", "5j", { desc = "Quit", noremap = true })
+map({ "n", "v" }, "K", "5k", { desc = "move up 5 lines", noremap = true })
+map({ "n", "v" }, "J", "5j", { desc = "move down 5 lines", noremap = true })
 
 map("n", "-", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
 map("n", "=", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
+
+-- window split
+map("n", "sl", "<cmd>:set nosplitright<CR>:vsplit<CR>:set splitright<CR>", { desc = "split to right" })
+map("n", "sj", "<cmd>:set nosplitbelow<CR>:split<CR>:set splitbelow<CR>", { desc = "split to bottom" })
+map("n", "<down>", "<cmd>:res +5<CR>", { desc = "resize up" })
+map("n", "<up>", "<cmd>:res -5<CR>", { desc = "resize up" })
+map("n", "<left>", "<cmd>:vertical resize-5<CR>", { desc = "resize up" })
+map("n", "<right>", "<cmd>:vertical resize+5<CR>", { desc = "resize up" })
