ed1c5f3f1c34210f6d5c948d063b869bf5ebca73
wangxin <vino0908@outlook.com>
2023-07-13 17:44:33 +0800

    修改telescope 键位

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,8 @@
+local Util = require("lazyvim.util")
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    { "<leader>fo", Util.telescope("oldfiles", { cwd = vim.loop.cwd() }), desc = "Recent (cwd)" },
+    { "<leader>fg", Util.telescope("live_grep"),                          desc = "Grep (root dir)" },
+  }
+}
